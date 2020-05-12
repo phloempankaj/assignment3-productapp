8b62d6df4c80043c6f9fb3d9752a3940c5cbc280
pankaj singh <phloem_pankaj@hotmail.com>
2020-05-12 18:37:03 +0530

    make changes for stackblitiz

--- src/app/components/product-form/product-form.component.ts
+++ src/app/components/product-form/product-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, OnChanges, Output, EventEmitter } from "@angular/core";
-import { Product } from "src/app/models/product";
+import { Product } from "../../models/product";
 
 @Component({
   selector: "app-product-form",
